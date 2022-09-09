21fdbc888bb95eb9e2e7fdaff769ba8de4bb1a5a
badlaand <caron.florian0@gmail.com>
2022-09-09 11:04:46 +0200

    bouton

---
+++ script.js
@@ -0,0 +1,12 @@
+function cache_affiche(ok1, okPlusMoins){
+        if(document.getElementById(ok1).style.display == 'none')
+        {
+                document.getElementById(ok1).style.display='block';
+                document.getElementById(okPlusMoins).value = '-';
+        }
+        else
+        {
+                document.getElementById(ok1).style.display='none';
+                document.getElementById(okPlusMoins).value = '+';
+        }
+    };
