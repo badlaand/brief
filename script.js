d79478fd5a720269d59173636ea1bd7e91a6bbd5
badlaand <caron.florian0@gmail.com>
2022-09-12 09:33:03 +0200

    remise en forme

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-function cache_affiche(ok1, okPlusMoins){
-        if(document.getElementById(ok1).style.display == 'none')
-        {
-                document.getElementById(ok1).style.display='block';
-                document.getElementById(okPlusMoins).value = '-';
-        }
-        else
-        {
-                document.getElementById(ok1).style.display='none';
-                document.getElementById(okPlusMoins).value = '+'
-        }
-    };
+function cache_affiche(ok1, okPlusMoins) {
+  if (document.getElementById(ok1).style.display == "none") {
+    document.getElementById(ok1).style.display = "block";
+    document.getElementById(okPlusMoins).value = "-";
+  } else {
+    document.getElementById(ok1).style.display = "none";
+    document.getElementById(okPlusMoins).value = "+";
+  }
+}
